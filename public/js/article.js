e5169fd73ef047b3b1cb3c774864531158774a8d
dnphu <phu.ngoc.dang@mgm-tp.com>
2021-06-26 17:06:20 +0700

    Finish push image to server

--- public/js/article.js
+++ public/js/article.js
@@ -1,3 +1,20 @@
 document.getElementById('upload').addEventListener('change', async function(e) {
+    const previewImg = document.getElementById('previewImg');
+    previewImg.src = URL.createObjectURL(this.files[0]);
+    const form = new FormData();
+    form.append('file', this.files[0])
     console.log(e);
+
+    const response = await fetch('http://localhost:3000/media', {
+        method: 'POST',
+        body: form
+    })
+
+    if (!response.ok) {
+        alert('Upload failed')
+    } else {
+        const {src} = await response.json();
+        alert(src);
+        return;
+    }
 })
